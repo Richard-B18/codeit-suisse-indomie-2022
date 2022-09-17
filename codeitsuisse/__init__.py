97006b2c3db32981cf4a067381a7449317d5e16a
richardbryan <richard.bryan1680@gmail.com>
2022-09-17 17:31:06 +0800

    debug

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+import logging
 app = Flask(__name__)
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
 import codeitsuisse.routes.square
 import codeitsuisse.routes.tickerStreamPart1
 import codeitsuisse.routes.tickerStreamPart2
