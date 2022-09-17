c07e05459d0a9211da592b560baeff5b597395c6
richardbryan <richard.bryan1680@gmail.com>
2022-09-17 17:33:52 +0800

    debug

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -1,11 +1,19 @@
-from flask import Flask
+import os
+import sys
 import logging
+from flask import Flask
+
 app = Flask(__name__)
-app.logger.addHandler(logging.StreamHandler(sys.stdout))
+if 'DYNO' in os.environ:
+    app.logger.addHandler(logging.StreamHandler(sys.stdout))
+    app.logger.setLevel(logging.ERROR)
+
 import codeitsuisse.routes.square
 import codeitsuisse.routes.tickerStreamPart1
 import codeitsuisse.routes.tickerStreamPart2
 import codeitsuisse.routes.travelling_suisse_robot
+
+
 
 
 
