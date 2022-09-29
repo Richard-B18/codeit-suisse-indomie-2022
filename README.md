d0c74686f599afb1e8451635a8237524dbd07a13
BrandonTay2001 <brandontay2001@gmail.com>
2022-09-29 16:34:51 +0800

    committed changes 3

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
+This template was edited from a Flask project by Brandon Tay (GitHub: BrandonTay2001) and Richard Bryan (GitHub: Richard-B18)
+
 Note the __init__.py file in each folder. This file makes python treat directories containing it to be loaded in a module
 
 Leave __init__.py in endpoints/routes empty!
-Remember to add new files in /endpoints/__init__.py
+Remember to add new files in /endpoints/__init__.py!
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ logger = logging.getLogger(__name__)
 def default_route():
     return "Python Template"
 
-
+# logging information
 logger = logging.getLogger()
 handler = logging.StreamHandler()
 formatter = logging.Formatter(
@@ -17,11 +17,11 @@ logger.addHandler(handler)
 logger.setLevel(logging.INFO)
 
 
-
+# server handling
 if __name__ == "__main__":
     logging.info("Starting application ...")
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind(('localhost', 8080))  # defaulted to port 5000
+    sock.bind(('localhost', 8080))  # defaulted to port 8080
     port = sock.getsockname()[1]
     sock.close()
-    app.run(port=port)
+    app.run(port=port, debug=True)  # debug mode is on
